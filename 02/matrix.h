1f6cc6216c7cfcafe85208c1bb80fcb0f92b0eeb
Ivan Yakovenko <ivanyakovenko@MacBook-Pro-Ivan.local>
2019-03-26 19:37:55 +0300

    Fix init, move-constructors, and some issues

--- 02/matrix.h
+++ 02/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
 class Matrix
 {
 private:
@@ -10,17 +11,16 @@ private:
 		size_t columns;
 		std::vector<int> row;
 	public:
-		Row(size_t columns_num = 0) : columns(columns_num)
+		Row(size_t columns_num) : columns(columns_num), row(std::vector<int>(columns_num))
 		{
-			row = std::vector<int>(columns_num);
 		}
-		Row(const Row& another) : columns(another.getColumns())
+		Row(const Row& another) : columns(another.getColumns()), row(std::vector<int>(another.row))
 		{
-			row = std::vector<int>(another.row);
 		}
-		Row(Row&& another) : columns(another.getColumns())
+		Row(Row&& another)
 		{
-			row = std::vector<int>(another.row);
+			std::swap(columns, another.columns);
+			row.swap(another.row);
 		}
 		size_t getColumns() const
 		{
@@ -28,14 +28,14 @@ private:
 		}
 		const int& operator[](size_t i) const
 		{
-			if(i< 0 || i >= columns)
+			if(i >= columns)
 				throw std::out_of_range("Out of size");
 			else
 				return row[i];
 		}
 		int& operator[](size_t i)
 		{
-			if(i< 0 || i >= columns)
+			if(i >= columns)
 				throw std::out_of_range("Out of size");
 			else
 				return row[i];
@@ -48,17 +48,17 @@ private:
 		}
 		Row& operator=(Row&& another)
 		{
-			columns = another.getColumns();
-			row = std::vector<int>(another.row);
+			std::swap(columns, another.columns);
+			row.swap(another.row);
 			return *this;
 		}
-		Row& operator*=(const int i)
+		Row& operator*=(int i)
 		{
 			for(int k = 0; k < getColumns(); k++)
 				row[k] *= i;
 			return *this;
 		}
-		const bool operator==(const Row& another) const
+		bool operator==(const Row& another) const
 		{
 			if(columns != another.getColumns())
 				return false;
@@ -67,7 +67,7 @@ private:
 					return false;
 			return true;
 		}
-		const bool operator!=(const Row& another) const
+		bool operator!=(const Row& another) const
 		{
 			return !(*this == another);
 		}
@@ -79,67 +79,60 @@ private:
 	size_t columns;
 	std::vector<Row> data;
 public:
-	Matrix(size_t row_num, size_t columns_num) : rows(row_num), columns(columns_num)
+	Matrix(size_t row_num, size_t columns_num) : rows(row_num), columns(columns_num), data(std::vector<Row>(rows,columns))
 	{
-		data = std::vector<Row>(rows);
-		for (size_t i = 0; i < rows; i++)
-			data[i] = Row(columns);
 	}
-	Matrix(const Matrix& another)
+	Matrix(const Matrix& another) : rows(another.getRows()), columns(another.getColumns()), data(std::vector<Row>(another.data))
 	{
-		columns = another.getColumns();
-		rows = another.getRows();
-		data = std::vector<Row>(another.data);
 	}
 	Matrix(Matrix&& another)
 	{
-		columns = another.getColumns();
-		rows = another.getRows();
-		data = std::vector<Row>(another.data);
+		std::swap(rows, another.rows);
+		std::swap(columns, another.columns);
+		data.swap(another.data);
 	}
 	size_t getRows() const
 	{
-		return this->rows;
+		return rows;
 	}
 	size_t getColumns() const
 	{
-		return this->columns;
+		return columns;
 	}
 	const Row& operator[](size_t i) const
 	{
-		if (i< 0 || i >= rows)
+		if (i >= rows)
 			throw std::out_of_range("Out of size");
 		return data[i];
 	}
 	Row& operator[](size_t i)
 	{
-		if (i< 0 || i >= rows)
+		if (i >= rows)
 			throw std::out_of_range("Out of size");
 		return data[i];
 	}
 	Matrix& operator=(const Matrix& another)
 	{
-		columns = another.getColumns();
 		rows = another.getRows();
+		columns = another.getColumns();
 		data = std::vector<Row>(another.data);
 		return *this;
 	}
 	Matrix& operator=(Matrix&& another)
 	{
-		columns = another.getColumns();
-		rows = another.getRows();
-		data = std::vector<Row>(another.data);
+		std::swap(rows, another.rows);
+		std::swap(columns, another.columns);
+		data.swap(another.data);
 		return *this;
 	}
-	Matrix& operator*=(const int i)
+	Matrix& operator*=(int i)
 	{
 		for(int k = 0; k < getRows(); k++)
 			data[k] *= i;
 		return *this;
 	}
-	const bool operator==(const Matrix& another) const
+	bool operator==(const Matrix& another) const
 	{
-		
 		if(columns != another.getColumns() || rows != another.getRows())
 			return false;
 		for(int k = 0; k < getRows(); k++)
@@ -147,7 +140,7 @@ public:
 				return false;
 		return true;
 	}
-	const bool operator!=(const Matrix& another) const
+	bool operator!=(const Matrix& another) const
 	{
 		return !(*this == another);
 	}
