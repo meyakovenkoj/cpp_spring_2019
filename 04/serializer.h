ae95b5d56e92c29a8b494b4f46bd099c0f148ecb
Иван Яковенко <ivan.yakovenko@icloud.com>
2019-04-07 14:08:00 +0300

    all done

--- 04/serializer.h
+++ 04/serializer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 enum class Error
 {
@@ -8,10 +9,8 @@ enum class Error
 
 class Serializer
 {
-	static constexpr char Separator = ' ';
 public:
-	explicit Serializer(std::ostream& out)
-		: out_(out)
+	explicit Serializer(std::ostream& out) : out_(out)
 	{
 	}
 
@@ -24,131 +23,108 @@ public:
 	template <class... ArgsT>
 	Error operator()(ArgsT... args)
 	{
-		return process(args...);
+		return process(std::forward<ArgsT>(args)...);
+	}
+private:
+	static constexpr char Separator = ' ';
+	std::ostream & out_;
+	
+	Error process(bool val)
+	{
+		out_ << (val ? "true" : "false") << Separator;
+		return Error::NoError;
 	}
 	
-private:
+	Error process(uint64_t & val) const
+	{
+		out_ << val << Separator;
+		return Error::NoError;
+	}
 	
+	template<class T>
+	Error process(T&& value)
+	{
+		return Error::CorruptedArchive;
+	}
+	
+	template<class T, class... ArgsT>
+	Error process(T&& value, ArgsT&& ... args)
+	{
+		if (process(std::forward<T>(value)) == Error::CorruptedArchive)
+		{
+			return Error::CorruptedArchive;
+		}
+		else
+		{
+			return process(std::forward<ArgsT>(args)...);
+		}
+	}
 };
 
 class Deserializer
 {
-	static constexpr char Separator = ' ';
 public:
-	explicit Deserializer(std::istream& in)
-		: in_(in)
+	explicit Deserializer(std::istream& in) : in_(in)
 	{
 	}
-
+	
 	template <class T>
 	Error load(T& object)
 	{
-		return object.deserialize(*this);
-	}
-
-	template <class... ArgsT>
-	Error operator()(ArgsT... args)
-	{
-		return process(args...);
+		return object.serialize(*this);
 	}
 	
-private:
-	// process использует variadic templates
-};
-
-Error stouint(std::string & input, uint64t & res)
-{
-	char buf;
-	for(size_t i = 0; i < input.length(); i++)
+	template <class... ArgsT>
+	Error operator()(ArgsT&&... args)
 	{
-		buf = input[i];
-		if(buf >= '0' && buf <= '9'){
-			res *= 10;
-			res += buf - '0';
+		return process(std::forward<ArgsT>(args)...);
+	}
+private:
+	static constexpr char Separator = ' ';
+	std::istream & in_;
+	
+	Error process(bool & val)
+	{
+		std::string input;
+		in_ >> input;
+		
+		if(!input.compare("true"))
+			val = true;
+		else if(!input.compare("false"))
+			val = false;
+		else
+			return Error::CorruptedArchive;
+		return Error::NoError;
+	}
+	
+	Error process(uint64_t & val)
+	{
+		std::string input;
+		in_ >> input;
+		val = 0;
+		
+		for(auto buf: input)
+		{
+			if(buf >= '0' && buf <= '9'){
+				val *= 10;
+				val += buf - '0';
+			}
+			else
+				return Error::CorruptedArchive;
+		}
+		return Error::NoError;
+	}
+	
+	template<class T, class... ArgsT>
+	Error process(T&& value, ArgsT&&... args)
+	{
+		if (process(std::forward<T>(value)) == Error::CorruptedArchive)
+		{
+			return Error::CorruptedArchive;
 		}
 		else
 		{
-			res = 0;
-			return Error::CorruptedArchive;
+			return process(std::forward<ArgsT>(args)...);
 		}
 	}
-	return Error::NoError;
-}
-
-void uinttos(std::string & output, uint64t & val)
-{
-	output = std::to_string(val);
-}
-
-Error stobool(std::string & input, bool & res)
-{
-	if(!input.compare("true"))
-		res = true;
-	else if(!input.compare("false"))
-		res = false;
-	else
-		return Error::CorruptedArchive;
-	return Error::NoError;
-}
-
-void booltos(std::string & output, bool & val)
-{
-	if(val)
-		output = "true";
-	else
-		output = "false";
-}
-
-Error split_str(std::string & line, std::vector<std::string> & vect)
-{
-	// int i = 0;
-	// stringstream ssin(line);
-	// std::string buf;
-	// while (ssin.good() && !ssin.eof()){
-	//	 ssin >> buf;
-	//	 vect(buf);
-	//	 buf.clear();
-	//	 ++i;
-	// }
-	std::string buf;
-	while(line[i] != '\0')
-	{
-		while(line[i] != Separator)
-		{
-			//add(line[i])
-			buf += line[i];
-			i++;
-		}
-		vect.push_back(buf);
-		buf.clear();
-		i++;
-		j++;
-	}
-	if (i != 3)
-		return Error::CorruptedArchive;
-	else
-		return Error::NoError;
-}
-
-Error load_obj(T& object)
-{
-	std::vector<std::string> vect;
-	if(split_str(vect) == Error::NoError)
-	{
-		try
-		{
-			for(int i = 0; i < vect.size(); i++)
-				if(vect[i][0] >= '0' && vect[i][0] <= '9')
-					stouint(vect[i]);
-				else
-					stobool(vect[i]);
-		}
-		catch(Error::CorruptedArchive)
-		{
-			return Error::CorruptedArchive;
-		}
-	}
-	return Error::NoError;
-}
-
-Error save()
+};
